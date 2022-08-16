0535af976b244322ebad4ed26a328dc3f5ba2549
Joe Eltgroth <jeltgroth@vmware.com>
2022-08-16 15:13:55 -0500

    WIP

--- function-dir/index.js
+++ function-dir/index.js
@@ -1,5 +1,5 @@
 module.exports = async function myFunction(context) {
-  const ret = 'This is a test function for Node.js FaaS. Success. Test';
+  const ret = 'This is a test function for Node.js FaaS. Success. Test2';
   return new Promise((resolve, reject) => {
     setTimeout(_ => {
       context.log.info('sending response to client')
